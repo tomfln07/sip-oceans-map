42674002493d341b3ce20918c82da5da1930847b
Lks <taumfoulon@gmail.com>
2024-11-01 18:40:10 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import { Map } from './comp/map/map'
+import "./style.css"
+
+export default function App() {
+	return (
+		<>
+			<Map/>
+		</>
+  	)
+}
---
+++ src/comp/map/map.jsx
@@ -0,0 +1,83 @@
+import { useEffect, useRef, useState } from "react"
+import { Item } from "./item/item";
+import "./style.css"
+import world from "../../assets/wrld.jpg"
+
+export function Map() {
+	const coords = useRef([0, 0]);
+	const mouse_pos_origin = useRef(null);
+	const zoom = useRef(1);
+	const world_img = useRef(null);
+
+	/*useEffect(() => {
+		update_world_img_pos(coords.current);
+	}, []);*/
+
+	useEffect(() => {
+		const mouse_down = (e) => {
+			if (e.button == "0") {
+				mouse_pos_origin.current = [e.screenX, e.screenY];
+				window.addEventListener("mousemove", track_mouse);
+			}
+		}
+
+		const mouse_up = (e) => {
+			if (e.button == "0") {
+				window.removeEventListener("mousemove", track_mouse);
+				mouse_pos_origin.current = null;
+			}
+		}
+
+		const wheel = (e) => {
+			if (e.deltaY < 0) {
+				zoom.current += 0.1;
+			} else if (zoom.current > 0) {
+				zoom.current -= 0.1;
+			}
+			if (zoom.current < 0) zoom.current = 0;
+			update_world_img_zoom();
+		}
+
+		window.addEventListener("mousedown", mouse_down);
+		window.addEventListener("mouseup", mouse_up);
+		//window.addEventListener("wheel", wheel);
+
+		return () => {
+			window.removeEventListener("mousedown", mouse_down);
+			window.removeEventListener("mouseup", mouse_up);
+			window.removeEventListener("mousemove", track_mouse);
+			window.removeEventListener("wheel", wheel);
+		}
+	}, [])
+	
+	const track_mouse = (e) => {
+		if (!mouse_pos_origin) return;
+		const delta_x = mouse_pos_origin.current[0] - e.screenX;
+		const delta_y = mouse_pos_origin.current[1] - e.screenY;
+		coords.current = [coords.current[0] + delta_x, coords.current[1] + delta_y];
+		update_world_img_pos(coords.current);
+		mouse_pos_origin.current = [e.screenX, e.screenY];
+	}
+
+	const update_world_img_pos = (pos) => {
+		if (world_img) {
+			world_img.current.style.top = `${-pos[1]}px`;
+			world_img.current.style.left = `${-pos[0]}px`;
+		}
+	}
+
+	const update_world_img_zoom = () => {
+		if (world_img) {
+			world_img.current.style.transform = `scale(${zoom.current})`;
+		}
+	}
+
+	return (
+		<>
+			<div style={{ height: `100vh`, width: `100vw` }} id="map_container">
+				<img src={world} id="world" ref={world_img}/>
+				<Item x={50} y={50} coords_ref={coords}/>
+			</div>
+		</>
+	)
+}
