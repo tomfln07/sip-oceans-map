643c6eafee1975c074f61e8ce40d58282a710600
tomfln <tom.foulon@epitech.eu>
2024-11-01 21:49:21 +0100

    fixed grab img bug, coords relative to the center of the screen & default pos to EU

---
+++ src/comp/item/item.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from "react"
+import "./style.css"
+
+export function Item({ x, y, coords_ref }) {
+	const item_ref = useRef(null);
+
+	useEffect(() => {
+		const id = setInterval(() => {
+			if (item_ref) {
+				item_ref.current.style.top = `${-coords_ref.current[1] + y}px`;
+				item_ref.current.style.left = `${-coords_ref.current[0] + x}px`;
+			}
+		}, 50);
+
+		return () => {
+			clearInterval(id);
+		}
+	}, []);
+
+	return (
+		<div className="item" ref={item_ref} style={{ zIndex: "3" }}>
+			<h1>ITEM</h1>
+		</div>
+	)
+}
--- src/comp/map/map.jsx
+++ src/comp/map/map.jsx
@@ -1,22 +1,23 @@
 import { useEffect, useRef, useState } from "react"
-import { Item } from "./item/item";
+import { Item } from "../item/item";
 import "./style.css"
-import world from "../../assets/wrld.jpg"
+import world from "../../assets/8081_earthmap10k.jpg"
 
 export function Map() {
-	const coords = useRef([0, 0]);
+	const coords = useRef([5600, 1342]);
 	const mouse_pos_origin = useRef(null);
 	const zoom = useRef(1);
 	const world_img = useRef(null);
+    var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
 
-	/*useEffect(() => {
+	useEffect(() => {
 		update_world_img_pos(coords.current);
-	}, []);*/
+	}, [window.innerHeight, window.innerWidth]);
 
 	useEffect(() => {
 		const mouse_down = (e) => {
 			if (e.button == "0") {
-				mouse_pos_origin.current = [e.screenX, e.screenY];
+				mouse_pos_origin.current = [e.clientX, e.screenY];
 				window.addEventListener("mousemove", track_mouse);
 			}
 		}
@@ -38,31 +39,39 @@ export function Map() {
 			update_world_img_zoom();
 		}
 
+        function preventDefault(e) {
+            e.preventDefault();
+        }
+
 		window.addEventListener("mousedown", mouse_down);
 		window.addEventListener("mouseup", mouse_up);
-		//window.addEventListener("wheel", wheel);
+		window.addEventListener(wheelEvent, preventDefault);
+        window.addEventListener("resize", () => update_world_img_pos(coords.current))
 
 		return () => {
 			window.removeEventListener("mousedown", mouse_down);
 			window.removeEventListener("mouseup", mouse_up);
 			window.removeEventListener("mousemove", track_mouse);
-			window.removeEventListener("wheel", wheel);
+			window.removeEventListener(wheelEvent, preventDefault);
+            window.removeEventListener("resize", update_world_img_pos)
+			//window.removeEventListener("wheel", wheel);
 		}
 	}, [])
 	
 	const track_mouse = (e) => {
 		if (!mouse_pos_origin) return;
-		const delta_x = mouse_pos_origin.current[0] - e.screenX;
+		const delta_x = mouse_pos_origin.current[0] - e.clientX;
 		const delta_y = mouse_pos_origin.current[1] - e.screenY;
 		coords.current = [coords.current[0] + delta_x, coords.current[1] + delta_y];
 		update_world_img_pos(coords.current);
-		mouse_pos_origin.current = [e.screenX, e.screenY];
+		mouse_pos_origin.current = [e.clientX, e.screenY];
 	}
 
 	const update_world_img_pos = (pos) => {
-		if (world_img) {
-			world_img.current.style.top = `${-pos[1]}px`;
-			world_img.current.style.left = `${-pos[0]}px`;
+        console.log(pos);
+        if (world_img) {
+			world_img.current.style.top = `${window.innerHeight / 2 - pos[1]}px`;
+			world_img.current.style.left = `${window.innerWidth / 2 - pos[0]}px`;
 		}
 	}
 
@@ -75,6 +84,7 @@ export function Map() {
 	return (
 		<>
 			<div style={{ height: `100vh`, width: `100vw` }} id="map_container">
+                <div id="barrier"></div> {/* disallow user to drag-and-drop workd img (preventing bugs when moving on the map) */}
 				<img src={world} id="world" ref={world_img}/>
 				<Item x={50} y={50} coords_ref={coords}/>
 			</div>
